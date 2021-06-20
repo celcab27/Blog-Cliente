9fcb664e7b31593659f3727e3845c1af9be1cb1d
Celeste <celestecabrera.2711@gmail.com>
2021-06-20 19:17:46 -0300

    Solucionado el tema de mostrar las cards por fila

--- cliente-react/src/components/cardsHome.js
+++ cliente-react/src/components/cardsHome.js
@@ -3,53 +3,49 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, CardDeck } from "react-bootstrap";
 
 class CardsHomeComponent extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { posts: [] };
   }
 
-  componentWillMount()
-  {
+  componentWillMount() {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((json) =>
-    {
-        this.setState({posts: json})
-    }
-    );
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ posts: json });
+      });
   }
 
   render() {
     var postArray = [];
     var variant = "dark";
 
-    postArray = this.state.posts.map((post) =>
-    {
-        return (
-            <Card
-          bg={variant.toLowerCase()}
-          text={variant.toLowerCase() === "light" ? "dark" : "white"}
-          style={{ width: "18rem" }}
-          className="mb-2">
-          <Card.Header>Header</Card.Header>
-          <Card.Body>
-            <Card.Title>{variant} Card Title </Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        );
+    postArray = this.state.posts.map((post) => {
+      return (
+        <div className="col">
+          <Card
+            bg={variant.toLowerCase()}
+            text={variant.toLowerCase() === "light" ? "dark" : "white"}
+            style={{ width: "18rem" }}
+            className="h-100"
+          >
+            <Card.Header>Post</Card.Header>
+            <Card.Body>
+              <Card.Title>
+                {variant} {post.title}{" "}
+              </Card.Title>
+              <Card.Text></Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      );
     });
 
-    return(
-       <CardDeck>
-           {postArray}
-       </CardDeck> 
-    )
-  
+    return (
+      <CardDeck className="row row-cols-1 row-cols-md-3 g-4">
+        {postArray}
+      </CardDeck>
+    );
   }
 }
 
