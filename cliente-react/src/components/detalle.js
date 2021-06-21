6c6ef8372adfc1d4592ddb980ca27d59abfd1adc
Celeste <celestecabrera.2711@gmail.com>
2021-06-20 23:41:54 -0300

    Arreglado el error de la visualizacion de un recurso inexistente

--- cliente-react/src/components/detalle.js
+++ cliente-react/src/components/detalle.js
@@ -2,34 +2,50 @@ import React from "react";
 import { withRouter } from "react-router";
 
 class DetalleComponent extends React.Component {
-  constructor(props)
-  {
-      super(props);
-      this.state = {post: []};
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-  componentWillMount() {
+  componentDidMount() {
     const id = this.props.match.params.id;
-    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({ post: json });
-      });
+    this.fetchData(id);
   }
+  
+  fetchData(id)
+  {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      this.setState({ post: json });
+    });
+  }
+
   render() {
-        const post = this.state.post;
-        console.log(post);
+    const post = this.state.post;
+
+    console.log(post);
+
+    if(post != undefined && post.id)
+    {
+      return (
+        <div>
+          <ul>
+            <li>Title: {post.title}</li>
+            <li>Content: {post.body}</li>
+          </ul>
+        </div>)
+    } else {
         return(
-            <div>
-                <ul>
-                    <li>Title: {post.title}</li>
-                    <li>Content: {post.body}</li>
-                </ul>
-            </div>
+          <div>
+            <h3>Error. El recurso al que está intentando acceder no existe.</h3>
+          </div>
         )
+
+    
     }
-
-
+  }
 }
 
 export default withRouter(DetalleComponent);
